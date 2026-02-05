37b9fd990cc77ae0ddae440db2cdfdcb261874ba
Clarisse-12 <c.mukayiran@alustudent.com>
2026-02-05 17:02:18 +0200

    project set up,  and bottom navigation implementation

---
+++ alu_academic_assistant/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import './navigations/bottom.dart';
+import './screens/assignments_screen.dart';
+import './screens/dashboard_screen.dart';
+import './screens/schedule_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ALU Academic Assistant',
+      theme: ThemeData(
+        primaryColor: const Color(0xFF003366), // ALU blue
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF003366),
+          foregroundColor: Colors.white,
+        ),
+      ),
+      home: const BottonNav(),
+    ); 
+  }
+}
